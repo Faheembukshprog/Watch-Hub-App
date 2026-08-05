c734b247d2bf558157d04e180ef18fb537762e87
Faheembukshprog <fbux12@gmail.com>
2026-08-06 04:11:52 +0500

    Fix: map ConnectivityResult list stream to single result for connectivity_plus v6+

--- lib/shared/providers/connectivity_provider.dart
+++ lib/shared/providers/connectivity_provider.dart
@@ -1,7 +1,9 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-/// Exposes a stream of the latest [ConnectivityResult].
-final connectivityProvider = StreamProvider<ConnectivityResult>(
-  (ref) => Connectivity().onConnectivityChanged,
-);
+/// Exposes a stream of the latest primary [ConnectivityResult].
+final connectivityProvider = StreamProvider<ConnectivityResult>((ref) {
+  return Connectivity().onConnectivityChanged.map(
+    (results) => results.isNotEmpty ? results.first : ConnectivityResult.none,
+  );
+});
